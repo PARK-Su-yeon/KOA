725509ad65b3d913da42a4251eda05e49fff6b31
수연 <syeon0131@naver.com>
2023-01-16 13:00:07 +0900

    .

---
+++ src/middleware/logging.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ * 관리 어드민의 요쳥 ip, url을 단순히 출력해주는 로그
+ * @param {*} next 
+ */
+exports.myLogging=async(ctx,next)=>{
+    let clientIp=ctx.request.ip;
+    console.log(`${clientIp.replace("::ffff:","")}주소에서 요청 : ${ctx.originalUrl}`);
+}
---
+++ src/routes.js
@@ -0,0 +1,23 @@
+const Router = require("@koa/router");
+const router = new Router();
+
+const{myLogging}=require('./middleware/logging')
+
+const webController = require("./web/controller");
+const apiUserController = require("./api/user/controller");
+const apiFeedController = require("./api/feed/controller");
+
+router.use(myLogging)
+
+router.get("/", webController.home);
+router.get("/page/:page", webController.page);
+
+router.get("/api/user/:id", apiUserController.info);
+
+router.get("/api/feed", apiFeedController.index);
+router.post("/api/feed", apiFeedController.store);
+router.get("/api/feed/:id", apiFeedController.show);
+router.get("/api/feed/:id", apiFeedController.update);
+router.delete("/api/feed/:id", apiFeedController.delete);
+
+module.exports = router;
